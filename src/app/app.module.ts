5a7dcc30719306f6ff9f771ae45b8b3e09088ddb
PerottiIrene <ireneperotti29@gmail.com>
2022-12-30 17:11:19 +0100

    implementata pagina di welcome con moduli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from './core/auth/auth.interceptor';
+import { AuthModule } from './core/auth/auth.module';
+import { WelcomeComponent } from './features/welcome/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AuthModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -3,16 +3,25 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpHeaders
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService:AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+   //se url non include la login, la request fa una richiesta al token (gli passa il token ad ogni richiesta)
+   if(!request.url.includes("login")){
+    request=request.clone({
+      headers:new HttpHeaders({ Authorization:  `Bearer ${this.authService.getUserToken()}` })
+    });
   }
+  return next.handle(request);
+  }
+
 }
